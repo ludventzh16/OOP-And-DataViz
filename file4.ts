1a0eba7f3a602800795e3c2211a29dafa17d3c8e
Ludventz H <112753295+ludventzh16@users.noreply.github.com>
2023-11-23 11:48:01 +0300

    Add files via upload
    OOP Задание 2 и 3

---
+++ file4.ts
@@ -0,0 +1,26 @@
+'''
+
+
+
+
+    examples
+    
+        Hooray, you're here! The day just got better - enjoy the following tips!
+        Hurra, du bist hier! Der Tag ist gerettet - nutze die folgenden Tipps!
+    
+    
+        A section of text like this is known as a text segment. Start rockin' your translations now!
+     Eine Textpassage wie diese bezeichnen wir als Textsegment. Starte jetzt mit deinen Übersetzungen durch!
+    
+    
+        Arriba, Arriba! Andale, Andale! Be fast as Speedy Gonzales. Just hit TAB to save and go to the next text segment, once you're done.
+     Arriba, Arriba! Andale, Andale! Sei schneller als Speedy Gonzales. Springe mit TAB ins nächste Textsegment, sobald du fertig bist. Deine Änderungen werden automatisch gespeichert.
+    
+    
+        Made with ❤
+        ❤
+        lh-check { min: 10, max: 15 }
+    
+
+
+'''
---
+++ file5.ts
@@ -0,0 +1,26 @@
+'''
+
+
+
+
+    examples
+    
+        Hooray, you're here! The day just got better - enjoy the following tips!
+        Hurrah, tu es là. Ça fait une bonne journée - profite des renseignements suivants !
+    
+    
+        A section of text like this is known as a text segment. Start rockin' your translations now!
+     Un tel extrait est qualifié de segment du texte chez nous. C’est parti – commence tes traductions tout de suite/maintenant !
+    
+    
+        Arriba, Arriba! Andale, Andale! Be fast as Speedy Gonzales. Just hit TAB to save and go to the next text segment, once you're done.
+        Arriba, Arriba ! Andale, Andale ! Prends Speedy Gonzales de vitesse. Appuie sur TAB pour sauvegarder et continuer avec le segment prochain dès que tu as fini.
+    
+    
+        Made with ❤
+        ❤
+        lh-check { min: 10, max: 15 }
+    
+
+
+'''
